16ecfc3b70f1afe351bf730f798518aed502e54c
BesturogMestur <birgirsig02@gmail.com>
2023-02-14 10:51:59 +0000

    breyti IntStack í tivisbreytu

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -1,17 +1,22 @@
 package is.hi.hbv202g.ass5;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+
 public class IntStackTest {
-private IntStack stack;
+    private IntStack stack;
+
+    @Before
+    public void createEmptyStack() {
+        stack = new IntStack();
+    }
+
     @Test
     public void testNewStackIsNotFull() {
-        stack= new IntStack();
-
         assertFalse(stack.isFull());
     }
+
 
 }
